4402149c799663e03559c0ca0461fa4f8c4af568
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-06-08 21:27:12 +0300

    KŤＢσŤ-𝐊

--- scripts/cmds/sing.js
+++ scripts/cmds/sing.js
@@ -7,11 +7,11 @@
 
 module.exports = {
   config: {
-    name: "sing",
-    author: "Samir Œ",
+    name: "play",
+    author: "keithkeizzah",
     description: "Search and download music from YouTube",
-    category: "song",
-    usage: "song [title]",
+    category: "play",
+    usage: "Download [title]",
     usePrefix: true,
     role: 0
 
@@ -22,7 +22,7 @@ module.exports = {
 
     try {
       if (!search) {
-        return bot.sendMessage(chatId, `Please provide a search query. Usage: /sing song name`);
+        return bot.sendMessage(chatId, `Please provide a search query. Usage: /play song name`);
       }
 
       bot.sendMessage(chatId, `🔍 Searching for song: ${search}`);
